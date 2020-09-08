942b0b5e1db66fecf7009fc5c31da08ea8638d1f
jonathandsilva2 <jonathandsilva2@gmail.com>
2020-09-08 19:23:02 -0400

    several changes

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,22 @@
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
 });
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity('.main-carousel', {
   // options
-
 });
-const input = document.querySelector("#LOL")
-const button = document.querySelector("#LOL2");
+const input = document.querySelector('#text-input');
+const button = document.querySelector('#click-button');
 
-button.addEventListener("click", function(event) {
- 
-
-  if (input.value.includes("@")){
-    alert("Thank you for subscribing")
-    
+button.addEventListener('click', function (event) {
+  if (input.value.includes('@')) {
+    alert('Thank you for subscribing');
   } else {
-   alert("Please enter a valid email address.")
-   
-}});
+    alert('Please enter a valid email address.');
+  }
+});
